92f5a2078f0dae1e8133425aeca96a1354ed533f
MoLeighJackson <moleighjackson@gmail.com>
2018-08-02 17:16:10 -0400

    added placeholder images for activity_now_playing.xml

--- app/src/main/java/com/example/moleigh/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/moleigh/musicplayer/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.moleigh.musicplayer;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView numbers = (TextView) findViewById(R.id.track_name);
+
+// Set a click listener on the track name view
+        numbers.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the track name View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent playSongIntent = new Intent(MainActivity.this, NowPlayingActivity.class);
+                startActivity(playSongIntent);
+            }
+        });
+
     }
 
-    public void openNowPlayingScreen(View view){
-        Intent details = new Intent(this, NowPlayingActivity.class);
-        startActivity(details);
-    }
 }
