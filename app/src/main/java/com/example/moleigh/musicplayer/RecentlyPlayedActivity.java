6159357ad2825c85d6628fa4786be0045c454dde
MoLeighJackson <moleighjackson@gmail.com>
2018-08-02 21:48:06 -0400

    Added for loop for recently played songs

--- app/src/main/java/com/example/moleigh/musicplayer/RecentlyPlayedActivity.java
+++ app/src/main/java/com/example/moleigh/musicplayer/RecentlyPlayedActivity.java
@@ -2,6 +2,10 @@ package com.example.moleigh.musicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class RecentlyPlayedActivity extends AppCompatActivity {
 
@@ -11,10 +15,18 @@ public class RecentlyPlayedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recently_played);
 
         //create an array of songs
-        String[] tracks = new String[3];
-        tracks[0] = "This is a Jazz Space by Midnight North";
-        tracks[1] = "The Black Cat by Aaron Kenny";
-        tracks[2] = "Canal 3 by Quincas Moreira";
+        ArrayList<String> tracks = new ArrayList<String>();
+        tracks.add("This is a Jazz Space by Midnight North");
+        tracks.add("The Black Cat by Aaron Kenny");
+        tracks.add("Canal 3 by Quincas Moreira");
 
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        for (int index = 0; index < tracks.size(); index++) {
+
+            TextView trackView = new TextView(this);
+            trackView.setText(tracks.get(index));
+            rootView.addView(trackView);
+        }
     }
 }
