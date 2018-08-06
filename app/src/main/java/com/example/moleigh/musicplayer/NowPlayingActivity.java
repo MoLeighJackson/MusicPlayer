692e49e60fa6befa1f9228ce3fb0b26e346c68da
MoLeighJackson <moleighjackson@gmail.com>
2018-08-06 13:48:10 -0400

    Added play and pause buttons to NowPlayingActivity

--- app/src/main/java/com/example/moleigh/musicplayer/NowPlayingActivity.java
+++ app/src/main/java/com/example/moleigh/musicplayer/NowPlayingActivity.java
@@ -1,11 +1,13 @@
 package com.example.moleigh.musicplayer;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -26,6 +28,16 @@ public class NowPlayingActivity extends AppCompatActivity {
                 startActivity(showRecentlyPlayedScreen);
             }
         });
+
+        Button playButton = (Button) findViewById(R.id.play_btn);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void onClick(View v){
+                Toast.makeText(NowPlayingActivity.this, "Play", Toast.LENGTH_LONG).show();
+            }
+        });
+
 
     }
 
